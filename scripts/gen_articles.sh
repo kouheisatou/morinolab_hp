9dc5b44c400fced793138a3047e19ed682885dee
kouheisatou <koheisato0420@gmail.com>
2025-06-16 02:24:49 +0900

    chore: remove unused image and update media extraction path in gen_articles script

--- scripts/gen_articles.sh
+++ scripts/gen_articles.sh
@@ -39,7 +39,7 @@ for TYPE in "${CONTENT_TYPES[@]}"; do
            --from docx \
            --to html \
            --standalone \
-           --extract-media="./" \
+           --extract-media="${OUTPUT_DIR}" \
            --output "${OUTPUT_DIR}/article.html"
   done
   shopt -u nullglob
